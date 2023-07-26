3a01c73ca2bd99491d459875521fe5714fcb43db
jason <jason@furiosa.ai>
2023-07-26 07:20:37 +0000

    feat: add stable diffusion v1.5/2.1

---
+++ optimum/litmus/multimodal/__init__.py
@@ -0,0 +1,95 @@
+import copy
+from pathlib import Path
+from typing import Optional, Tuple, Union
+
+import onnx
+from optimum.litmus import compile_onnx, export_onnx, simplify_onnx, utils
+
+TASKS = ["text-generation-with-past"]
+BATCH_SIZE = 1
+INPUT_LENGTH = 77
+LATENT_HEIGHT = LATENT_WIDTH = 64
+
+
+def export(model_tag: str, output_dir: Union[Path, str], task: str, framework: str = "pt") -> None:
+    if task != "stable-diffusion":
+        raise Exception(f"Unsupported model task: {task}")
+    export_stable_diffusion_onnx(model_tag, output_dir, framework)
+
+
+def simplify(
+    input_model: Union[Path, onnx.ModelProto],
+    batch_size: int,
+    latent_shape: Tuple[int, int],
+    input_len: int,
+    time_step: int,
+    task: str = "stable-diffusion",
+) -> onnx.ModelProto:
+    if task != "stable-diffusion":
+        raise Exception(f"Unsupported model task: {task}")
+    model_opt = simplify_stable_diffusion_onnx(
+        input_model, batch_size, latent_shape, input_len, time_step
+    )
+    return model_opt
+
+
+def compile(model_dir: Path, task: str = "stable-diffusion") -> None:
+    if task != "stable-diffusion":
+        raise Exception(f"Unsupported model task: {task}")
+    compile_stable_diffusion_onnx(model_dir)
+
+
+def export_stable_diffusion_onnx(
+    model_tag: str, output_dir: Union[Path, str], framework: str = "pt"
+):
+    output_dir = Path(output_dir) if isinstance(output_dir, str) else output_dir
+    for module in ["text_encoder", "unet", "vae_decoder"]:
+        if not (output_dir / module / "model.onnx").exists():
+            print("Exporting ONNX...")
+            export_onnx(
+                model_name_or_path=model_tag,
+                output=output_dir,
+                task="stable-diffusion",
+                framework="pt",
+            )
+
+
+def simplify_stable_diffusion_onnx(
+    input_model: Path,
+    batch_size: int,
+    latent_shape: Tuple[int, int],
+    input_len: int,
+    time_step: int,
+):
+    input_shapes = {
+        "text_encoder": {"input_ids": [batch_size, input_len]},
+        "unet": {
+            "sample": [batch_size, 4, *latent_shape],
+            "timestep": [batch_size],
+            "encoder_hidden_states": [batch_size, input_len, 768],
+        },
+        "vae_decoder": {"latent_sample": [1, 4, *latent_shape]},
+    }
+
+    for module, fixed_input_shapes in input_shapes.items():
+        print(f"Simplifying {module} module...")
+        module_dir = input_model / module
+        if module == "unet":
+            onnx_model = utils.load_onnx(module_dir / "model.onnx")
+            hidden_state_input = onnx_model.graph.input[2]
+            assert hidden_state_input.name == "encoder_hidden_states"
+            hidden_state_dim = hidden_state_input.type.tensor_type.shape.dim[2].dim_value
+            fixed_input_shapes["encoder_hidden_states"][-1] = hidden_state_dim
+
+        simplify_onnx(module_dir / "model.onnx", module_dir / "model-opt.onnx", fixed_input_shapes)
+
+
+def compile_stable_diffusion_onnx(model_dir: Path) -> None:
+    for module in ["text_encoder", "unet", "vae_decoder"]:
+        print(f"Compiling {module} module...")
+        module_dir = model_dir / module
+        compile_onnx(
+            module_dir / "model-opt.onnx",
+            module_dir / "model-opt.dfg",
+            module_dir / "model-opt.dot",
+        )
--- optimum/litmus/multimodal/stable-diffusion.py
+++ optimum/litmus/multimodal/stable-diffusion.py
@@ -1,78 +1,78 @@
 import argparse
 from pathlib import Path
 
-from optimum.litmus import compile_onnx, export_onnx, utils
-from optimum.litmus.nlp import BATCH_SIZE, INPUT_LENGTH, TASKS, simplify
+from optimum.litmus import utils
+from optimum.litmus.multimodal import (
+    BATCH_SIZE,
+    INPUT_LENGTH,
+    LATENT_HEIGHT,
+    LATENT_WIDTH,
+    compile,
+    export,
+    simplify,
+)
 
 
 def main():
     parser = argparse.ArgumentParser("FuriosaAI litmus Stable Diffusion using HF Optimum API.")
     parser.add_argument("output_dir", type=Path, help="path to directory to save outputs")
     parser.add_argument(
-        "--version", "-v", choices=["xl-base", "xl-refiner", "1.5", "2.1"], required=True, help="available model sizes"
+        "--version",
+        "-v",
+        choices=["1.5", "2.1"],
+        required=True,
+        help="Available model versions",
     )
     parser.add_argument(
         "--batch-size",
         "-b",
         default=BATCH_SIZE,
         type=utils.check_non_negative,
-        help="Batch size for model inputs",
+        help="Batch size for latent and prompt inputs",
+    )
+    parser.add_argument(
+        "--latent_shape",
+        default=(LATENT_HEIGHT, LATENT_WIDTH),
+        nargs=2,
+        type=int,
+        metavar=("latent_height", "latent_width"),
+        help="Shape of latent input. Note it is 1/8 of output image sizes",
     )
     parser.add_argument(
         "--input-len",
         default=INPUT_LENGTH,
         type=utils.check_non_negative,
-        help="Length of input prommpt",
-    )
-    parser.add_argument(
-        "--gen-step",
-        default=0,
-        type=utils.check_non_negative,
-        help="Generation step to simplify onnx graph",
-    )
-    parser.add_argument(
-        "--task",
-        default="image-to-text",
-        type=str,
-        choices=TASKS,
-        help="Task to export model for",
+        help="Length of input prompt",
     )
     args = parser.parse_args()
 
     version = args.version
-    if version in ["xl-base", "xl-refiner"]:
-        model_tag = f"stabilityai/stable-diffusion-{version}-0.9"
-    elif version == "1.5":
+    if version == "1.5":
         model_tag = "runwayml/stable-diffusion-v1-5"
     elif version == "2.1":
         model_tag = "stabilityai/stable-diffusion-2-1"
     else:
         raise ValueError(f"Unsupported version: {version}")
 
+    task = "stable-diffusion"
     output_dir = args.output_dir
 
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
-    
-    print("Exporting ONNX Model...")
-    print(model_tag)
-    export_onnx(model_name_or_path=model_tag, output=output_dir, task=args.task, framework="pt")
 
-    # print("Simplifying ONNX Model...")
-    # simplify(
-    #     output_dir / "decoder_model_merged.onnx",
-    #     output_dir,
-    #     args.batch_size,
-    #     args.input_len,
-    #     args.gen_step,
-    #     args.task,
-    # )
+    export(model_tag=model_tag, output_dir=output_dir, task=task, framework="pt")
 
-    # compile_onnx(
-    #     output_dir / f"decoder_model-opt_gen_step={args.gen_step}.onnx",
-    #     output_dir / f"decoder_model-opt_gen_step={args.gen_step}.dfg",
-    #     output_dir / f"decoder_model-opt_gen_step={args.gen_step}.dot",
-    # )
+    print("Simplifying ONNX Model...")
+    latent_shape = tuple(args.latent_shape)
+    simplify(
+        output_dir,
+        args.batch_size,
+        latent_shape,
+        args.input_len,
+        task,
+    )
+
+    compile(output_dir)
 
 
 if __name__ == "__main__":
